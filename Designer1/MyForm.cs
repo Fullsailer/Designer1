0abe91c778240445de05e58d7c3fa1ce07fd4115
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-07-20 14:50:48 -0400

    Added new method action
    Added a new method action to show a message on the button click event.

--- Designer1/MyForm.cs
+++ Designer1/MyForm.cs
@@ -16,5 +16,10 @@ namespace Designer1
         {
             InitializeComponent();
         }
+
+        private void ShowMessageButton_Click(object sender, EventArgs e)
+        {
+            MessageLabel.Text = MessageTextBox.Text;
+        }
     }
 }
